23011919f1522d76aa9bd43036551cc839f6731a
George <kachergis@stanford.edu>
2019-10-25 17:15:53 -0700

    add individual learning rate variability

--- app.R
+++ app.R
@@ -5,6 +5,7 @@
 
 library(shiny)
 require(tidyverse)
+library(quantreg)
 
 # Define UI
 ui <- fluidPage(
@@ -12,13 +13,13 @@ ui <- fluidPage(
 
     sidebarLayout(
         sidebarPanel(
-            selectInput("distro", "Word difficulty distribution:",
+            selectInput("distro", "Word frequency distribution:",
                         list("Zipfian" = "zipf", 
                              "Uniform" = "uniform")),
             sliderInput("vocab_size", "Vocabulary size:", 
-                        min=100, max=10000, value=2000, step=200),
+                        min=200, max=10000, value=5000, step=200),
             sliderInput("n_learners", "Number of learners:", 
-                        min=1, max=1000, value=100, step=50),
+                        min=10, max=500, value=100, step=10),
             sliderInput("input_rate", "Input rate (words/day):", 
                         min=0, max=10000, value=1000, step=100),
             sliderInput("threshold", "Number of occurrences needed to learn a word:", 
@@ -26,7 +27,7 @@ ui <- fluidPage(
             sliderInput("max_age", "Age range (months):", 
                         min=0, max=48, value=24, step=3),
             sliderInput("learning_rate", "Mean learning rate:", 
-                        min = 0, max = 1, value = 0.5, step= 0.1),
+                        min = 1, max = 10, value = 1, step= 1),
             sliderInput("proc_speed", "Mean processing speed:", 
                         min = 0, max = 1, value = 0.5, step= 0.1),
             checkboxInput("proc_facilitates", "Processing facilitates acquisition", FALSE)
@@ -39,10 +40,14 @@ ui <- fluidPage(
     )
 )
 
+sigmoid <- function(x) {
+    return( 1 / (1 + exp(-x)) )
+}
+
 # need to add proc_speed term and proc_facilitates interaction term
-#simulate(1000, "zipf", 100, 10, 15, 12)
-simulate <- function(vocab_size, distro, input_rate, n_learners, threshold, max_age) {
-    # could add learning_rate = rnorm(n_learners, mean_learning_rate) # individual learning rates
+simulate <- function(vocab_size, distro, input_rate, n_learners, threshold, max_age, mean_learning_rate) {
+    learning_rate = rnorm(n_learners, mean=mean_learning_rate, sd=1) # individual learning rates
+    learning_rate[which(learning_rate<0)] = 0.00001 # >0
     if(distro=="zipf") {
         probs = 1:vocab_size / sum(1:vocab_size)
     } else if(distro=="uniform") {
@@ -60,17 +65,18 @@ simulate <- function(vocab_size, distro, input_rate, n_learners, threshold, max_
     for(t in 1:max_age) {
         # sample 1 month of tokens for all learners
         tokens = matrix(sample(1:vocab_size, tokens_per_mo*n_learners, prob=probs, replace=T), nrow=n_learners) 
-        mo_word_occs = apply(tokens, 1, tabulate) # occurences of each word this month per subject (column)
+        mo_word_occs = apply(tokens, 1, tabulate, nbins=vocab_size) # occurences of each word this month per subject (column)
         # mo_word_occs = mo_word_occs * learning_rate # learning rate scales value of occurrences
-        cumulative_word_occs = cumulative_word_occs + t(mo_word_occs) # accumulate occurrences this month
-        known_words[,t] = rowSums(cumulative_word_occs>threshold) # occasionally fails?
+        cumulative_word_occs = cumulative_word_occs + learning_rate*t(mo_word_occs) # accumulate occurrences this month
+        known_words[,t] = rowSums(cumulative_word_occs>threshold) 
     }
     # return per individual per month 
     # reshape to long
+    known_words = cbind(rep(0,s), known_words)
     known_words = data.frame(known_words)
-    names(known_words) = 1:max_age
+    names(known_words) = 0:max_age
     known_words$id = 1:nrow(known_words)
-    known_words_l = gather(known_words, "month", "words", 1:max_age) 
+    known_words_l = gather(known_words, "month", "words", 1:(max_age+1)) 
     known_words_l$month = as.numeric(as.character(known_words_l$month))
     return(known_words_l)
 }
@@ -79,12 +85,14 @@ simulate <- function(vocab_size, distro, input_rate, n_learners, threshold, max_
 # server logic
 server <- function(input, output) {
     output$ageVocab <- renderPlot({
-        age <- 0:input$max_age
-        sim = simulate(input$vocab_size, input$distro, input$input_rate, input$n_learners, input$threshold, input$max_age)
-        #sim = simulate(1000, "zipf", 100, 10, 15, 12)
+        sim = simulate(input$vocab_size, input$distro, input$input_rate, 
+                       input$n_learners, input$threshold, input$max_age, input$learning_rate)
+        #sim = simulate(5000, "uniform", 500, 10, 15, 12, 1)
         gd <- sim %>% group_by(month, id) %>% summarise(mean=mean(words), sd=sd(words))
-        
-        ggplot(sim, aes(x=month, y=words)) + geom_point(alpha=.7) + geom_smooth() + # position="jitter", 
+        qs <- c(0.10,0.25,0.50,0.75,0.90)
+        ggplot(sim, aes(x=month, y=words)) + geom_jitter(width=.1, alpha=.2) + geom_point(alpha=.2) + # position="jitter", 
+            geom_quantile(quantiles=qs, formula=y ~ poly(x, 2), aes(colour = as.factor(..quantile..))) + 
+            labs(colour="Quantile") +
             geom_abline(intercept=0, slope=input$vocab_size/input$max_age, linetype="dashed", color="grey", size=1) + 
             xlab("Age (months") + ylab("Vocabulary Size") + theme_bw() + 
             ylim(0,input$vocab_size) + xlim(0, input$max_age) 
