bcb8018e818613a073a1cfc54cc81ecc695b208e
George <kachergis@stanford.edu>
2019-10-28 14:55:05 -0700

    added RT by age

--- app.R
+++ app.R
@@ -24,22 +24,26 @@ ui <- fluidPage(
                         min=10, max=200, value=50, step=10),
             sliderInput("input_rate", "Input rate (tokens/day):", # HR welfare: 616, prof: 2153
                         min=100, max=6000, value=1000, step=100),
+            helpText("e.g., Hart & Risley low SES: 616; high SES: 2153"),
             sliderInput("threshold", "Number of occurrences needed to learn a word:", 
                         min=0, max=1000, value=100, step=10),
+            checkboxInput("threshold_varies", "Threshold varies (i.e., word difficulty normally distributed)"),
             sliderInput("max_age", "Age range (months):", 
                         min=0, max=48, value=36, step=3),
             sliderInput("learning_rate", "Mean learning rate (scales value of occurrence; truncated at .1):", 
                         min = 1, max = 10, value = 1, step= 1),
-            sliderInput("proc_speed", "Mean processing speed:", 
-                        min = 0, max = 1, value = 0.5, step= 0.1),
+            sliderInput("proc_speed_dev", "Mean processing speed rate of development:", 
+                        min = 0, max = 1, value = 0.72, step= 0.02),
             checkboxInput("proc_facilitates", "Processing facilitates acquisition", FALSE)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("ageVocab"),
-           textOutput("summary"),
-           DT::dataTableOutput("mytable")
+            tabsetPanel(type = "tabs", id="tabs", 
+                tabPanel("Vocabulary Growth by Age", plotOutput("ageVocab")),
+                tabPanel("Processing Speed by Age", plotOutput("ageRT")),
+                tabPanel("Vocabulary Growth Table", textOutput("summary"), DT::dataTableOutput("mytable"))
+            )
         )
     )
 )
@@ -49,19 +53,31 @@ ui <- fluidPage(
 server <- function(input, output) {
     sim_data = reactive({
         simulate(input$vocab_size, input$distro, input$input_rate, 
-            input$n_learners, input$threshold, input$max_age, input$learning_rate)
+            input$n_learners, input$threshold, input$max_age, 
+            input$learning_rate, input$threshold_varies, 
+            input$proc_facilitates, input$proc_speed_dev)
     })
     
     output$ageVocab <- renderPlot({
         #sim = simulate(input$vocab_size, input$distro, input$input_rate, 
         #               input$n_learners, input$threshold, input$max_age, input$learning_rate)
         qs <- c(0.10,0.25,0.50,0.75,0.90)
-        ggplot(sim_data(), aes(x=month, y=words)) + geom_jitter(width=.1, alpha=.1) + geom_point(alpha=.1) +  
+        ggplot(sim_data()$known_words, aes(x=month, y=words)) + geom_jitter(width=.1, alpha=.1) + geom_point(alpha=.1) +  
             geom_quantile(quantiles=qs, formula=y ~ poly(x, 2), aes(colour = as.factor(..quantile..))) + 
             labs(colour="Quantile") + geom_smooth() + 
             geom_abline(intercept=0, slope=input$vocab_size/input$max_age, linetype="dashed", color="grey", size=1) + 
-            xlab("Age (months") + ylab("Vocabulary Size") + theme_bw() + 
+            xlab("Age (months)") + ylab("Vocabulary Size") + theme_bw() + 
             ylim(0,input$vocab_size) + xlim(0, input$max_age) 
+    })
+    
+    output$ageRT <- renderPlot({
+        qs <- c(0.10,0.25,0.50,0.75,0.90)
+        ggplot(sim_data()$proc_speed, aes(x=month, y=words)) + geom_jitter(width=.1, alpha=.1) + geom_point(alpha=.1) +  
+            geom_quantile(quantiles=qs, formula=y ~ poly(x, 2), aes(colour = as.factor(..quantile..))) + 
+            labs(colour="Quantile") + geom_smooth() + 
+            geom_abline(intercept=0, slope=input$vocab_size/input$max_age, linetype="dashed", color="grey", size=1) + 
+            xlab("Age (months)") + ylab("Response Time (seconds)") + theme_bw() + 
+            xlim(0, input$max_age) + ylim(0,2)
     })
     
     output$summary <- renderText({ 
@@ -69,8 +85,11 @@ server <- function(input, output) {
     })
     
     output$mytable = DT::renderDataTable({
-        sim_data() %>% group_by(month) %>% summarise(mean=mean(words), sd=sd(words)) %>%
-            datatable() %>% formatRound(columns=c("mean","sd"), digits=0)
+        sim_data()$known_words %>% group_by(month) %>% 
+            summarise(mean=mean(words), sd=sd(words)) %>%
+            mutate(cumulative_tokens=input$input_rate*30.42*month) %>%
+            datatable(options = list(lengthMenu = c(6, 12, 24), pageLength=24)) %>% 
+            formatRound(columns=c("mean","sd"), digits=0)
     })
 }
 
--- model.R
+++ model.R
@@ -2,21 +2,40 @@ sigmoid <- function(x) {
   return( 1 / (1 + exp(-x)) )
 }
 
+make_long_df <- function(df, n_learners, max_age, zeroval) {
+  df = cbind(rep(zeroval,n_learners), df) # value at 0 mos
+  df = data.frame(df)
+  names(df) = 0:max_age
+  df$id = 1:nrow(df)
+  df_l = gather(df, "month", "words", 1:(max_age+1)) 
+  df_l$month = as.numeric(as.character(df_l$month))
+  return(df_l)
+}
 
 # need to add proc_speed term and proc_facilitates interaction term
-simulate <- function(vocab_size, distro, input_rate, n_learners, threshold, max_age, mean_learning_rate) {
+simulate <- function(vocab_size, distro, input_rate, n_learners, threshold, max_age, mean_learning_rate, threshold_varies, proc_facilitates, proc_speed_dev) {
   learning_rate = rnorm(n_learners, mean=mean_learning_rate, sd=1) # individual learning rates
   learning_rate[which(learning_rate<0)] = 0.1 # >0
   if(distro=="zipf") {
-    # should shuffle these if we will add difficulty distribution
     probs = 1 / (1:vocab_size + 2.7) # f(r) = 1 / (r+beta)^alpha, alpha=1, beta=2.7 (Mandelbrot, 1953, 1962)
     probs = probs/sum(probs)
+    probs = sample(probs, length(probs)) # shuffle
   } else if(distro=="uniform") {
     probs = rep(1/vocab_size, vocab_size)
   } 
   
+  # fixed threshold for all words, or normal distribution over word difficulty (like McMurray 2007)
+  if(threshold_varies) threshold = rnorm(vocab_size, mean=threshold, sd=1)
+  
   cumulative_word_occs = matrix(0, nrow=n_learners, ncol=vocab_size) # number of times each word has appeared per learner
   known_words = matrix(0, nrow=n_learners, ncol=max_age) # known words per individual (row) per month (col)
+  proc_speed = matrix(0, nrow=n_learners, ncol=max_age) # do we want per word instead of per learner?
+  
+  # processing speed
+  # Kail (1991): RT slopes follow an exponential, such that Y(i) = a + b*exp(−c*i), where Y=predicted var, i=age. 
+  a = 0.56 # eventual (adult) asymptote
+  b = 1.04 # multiplier for the (infant) intercept
+  c = proc_speed_dev #rate of development (0.72 in Frank, Lewis, & MacDonald, 2016)
   
   time_steps = round(30.42*max_age) # days/month * age
   # sample for all learners and time_steps at once
@@ -27,20 +46,25 @@ simulate <- function(vocab_size, distro, input_rate, n_learners, threshold, max_
     # sample 1 month of tokens for all learners
     tokens = matrix(sample(1:vocab_size, tokens_per_mo*n_learners, prob=probs, replace=T), nrow=n_learners) 
     mo_word_occs = apply(tokens, 1, tabulate, nbins=vocab_size) # occurences of each word this month per subject (column)
-    # mo_word_occs = mo_word_occs * learning_rate # learning rate scales value of occurrences
-    cumulative_word_occs = cumulative_word_occs + learning_rate*t(mo_word_occs) # accumulate occurrences this month
+    
+    proc_speed[,t] = a + rowSums(b*exp(-c*log(cumulative_word_occs+1))) / vocab_size
+    
+    # learning rate scales value of occurrences
+    if(proc_facilitates) { # further scale value of word occurrences by processing speed
+      cumulative_word_occs = cumulative_word_occs + (3-proc_speed[,t])*learning_rate*t(mo_word_occs)
+    } else {
+      cumulative_word_occs = cumulative_word_occs + learning_rate*t(mo_word_occs) # accumulate occurrences this month
+    }
     known_words[,t] = rowSums(cumulative_word_occs>threshold) 
   }
-  # return per individual per month 
+  
+  # return known words and mean RT per individual per month 
   # reshape to long
-  known_words = cbind(rep(0,n_learners), known_words)
-  known_words = data.frame(known_words)
-  names(known_words) = 0:max_age
-  known_words$id = 1:nrow(known_words)
-  known_words_l = gather(known_words, "month", "words", 1:(max_age+1)) 
-  known_words_l$month = as.numeric(as.character(known_words_l$month))
-  return(known_words_l)
+  known_words_l = make_long_df(known_words, n_learners, max_age, 0)
+  proc_speed_l = make_long_df(proc_speed, n_learners, max_age, a+exp(-c))
+  return(list(known_words=known_words_l, proc_speed=proc_speed_l))
 }
 
-#sim = simulate(5000, "uniform", 500, 10, 15, 12, 1)
+# vocab_size, distro, input_rate, n_learners, threshold, max_age, mean_learning_rate, threshold_varies, proc_facilitates, proc_speed_dev)
+#sim = simulate(5000, "uniform", 500, 10, 15, 12, 1, F, F, .72)
 #sim %>% group_by(month) %>% summarise(mean=mean(words), sd=sd(words))
